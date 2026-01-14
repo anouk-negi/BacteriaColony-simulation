5b4f60dfbb887dcc8fc745b7a23100cf1776c01d
anouk-negi <anouk.negi@student.upt.ro>
2026-01-14 22:33:24 +0200

    Fixed logic erros

--- bacteria_colony.c
+++ bacteria_colony.c
@@ -6,11 +6,11 @@
 
 #define MAX_FILENAME 256
 
-char* grid;
-char* new_grid;
+char* initial_grid;
 char* groundtruth;
+
 int n,m;
-int thread_count, nr_gens;
+int nr_gens;
 
 void get_output_filename(char *input, char *output)
 {
@@ -26,12 +26,9 @@ void get_output_filename(char *input, char *output)
     sprintf(output, "%s_out.txt", base);
 }
 
-void read_grid(char* input_filename, char** local_grid, int* n, int* m, int my_rank, int comm_sz)
+void read_init_grid(char* input_filename, int* n, int* m, int my_rank, int comm_sz)
 {
-    char* whole_grid = NULL;
-
     if (my_rank == 0) {
-        
         FILE* fin = fopen(input_filename, "r");
         if(fin == NULL)
         { 
@@ -44,56 +41,77 @@ void read_grid(char* input_filename, char** local_grid, int* n, int* m, int my_r
             fclose(fin);
             return;
         }
+
+        initial_grid = (char*) malloc((*n) * (*m) *sizeof(char));
+        if(initial_grid == NULL)
+        {
+            printf("Rank %d: Cannot allocate initial grid\n",my_rank);
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
+
+        for(int i=0; i< (*n)* (*m); i++)
+        {
+            fscanf(fin, " %c", &initial_grid[i]);
+        }
         fclose(fin);
     }
         
     MPI_Bcast(n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+}
 
-    int local_rows = (*n) / comm_sz;
-    * local_grid = (char*)malloc(local_rows * (*m) * sizeof(char));
 
-    if (my_rank == 0) {
-        whole_grid = (char*)malloc((*n) * (*m) * sizeof(char));
-        FILE* fin = fopen(input_filename, "r");
-        int dummy_n, dummy_m;
-            
-        fscanf(fin, "%d %d", &dummy_n, &dummy_m);
-
-        for (int i = 0; i < (*n) * (*m); i++) {
-            fscanf(fin, " %c", &whole_grid[i]);
+void allocate_and_init(int rank, char **grid, char **new_grid, int n, int m, int comm_sz)
+{
+    char *whole_grid = NULL;
+    if (rank == 0)
+    {
+        whole_grid = (char*) malloc(n * m * sizeof(char));
+        if (whole_grid == NULL)
+        {
+            printf("Rank %d: Cannot allocate whole grid\n", rank);
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
         }
-        fclose(fin);
+        //init grid
     }
 
-    MPI_Scatter(whole_grid, local_rows * (*m), MPI_CHAR, 
-            *local_grid, local_rows * (*m), MPI_CHAR, 
+    int local_rows = n / comm_sz;
+    //allocate local grids
+    if(rank == comm_sz - 1)
+    {
+        local_rows = n % comm_sz;
+    }
+
+    int local_N = local_rows + 2;
+    
+    *grid = (char*)malloc(n * local_N * sizeof(char));
+    *new_grid = (char*)malloc(n * local_N * sizeof(char));
+    if ((*grid == NULL) || (*new_grid == NULL))
+    {
+        printf("Rank %d: Cannot allocate local grid\n", rank);
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    //starting parallel
+
+    MPI_Scatter(whole_grid, local_rows * m, MPI_CHAR, 
+            (*grid) + m, local_rows * m, MPI_CHAR, 
             0, MPI_COMM_WORLD);
 
-    if (my_rank == 0) free(whole_grid);
+    if (rank == 0) free(whole_grid);
 }
 
 void print_global_grid(char* local_grid, int m, int n, int my_rank, int comm_sz) {
-    int local_rows = n / comm_sz;
-    char* final_grid = NULL;
-
-    if (my_rank == 0) {
-        final_grid = (char*)malloc(n * m * sizeof(char));
-    }
-
-    MPI_Gather(local_grid, local_rows * m, MPI_CHAR,
-               final_grid, local_rows * m, MPI_CHAR,
-               0, MPI_COMM_WORLD);
-
     if (my_rank == 0) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                printf("%c", final_grid[i * m + j]);
+                printf("%c", local_grid[i * m + j]);
             }
             printf("\n");
         }
-        free(final_grid);
     }
+    
+    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 int main(int argc, char *argv[])
@@ -113,16 +131,21 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if(my_rank == 0)
+    char *grid, *new_grid; 
+
+    /*if(my_rank == 0)
     {
         get_output_filename(argv[1], output_file);
+    }*/
+
+    read_init_grid(argv[1], &n, &m, my_rank, comm_sz);
+
+    print_global_grid(initial_grid,n,m,my_rank,comm_sz);
+
+    if (my_rank == 0 && initial_grid != NULL) {
+        free(initial_grid);
     }
 
-    
-    read_grid(argv[1], &local_slice, &n, &m, my_rank, comm_sz);
-
-    print_global_grid(local_slice,n,m,my_rank,comm_sz);
-    free(local_slice);
-
     MPI_Finalize();
+
 }
